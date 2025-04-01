8090844266d86c24fd9d6b2b535edb89f65f7ba1
le Seb du fond <113973829+SebastienBorloz@users.noreply.github.com>
2025-04-01 08:51:42 +0200

    added package files for phys_simulator

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="phys_sim",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "pybullet",
+    ],
+    python_requires=">=3.10,<3.12",
+    author="6 Figures",
+    description="A package to simulate the ur3e robot arm physics for security usages",
+    package_data={
+        "camera_sync": [
+            "simulator/urdfs/*",
+        ],
+    },
+)
